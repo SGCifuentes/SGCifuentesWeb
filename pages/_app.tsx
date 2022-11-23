ff0955e174acabdb38dad20aec96c63e73ccc682
Sebastian.gc <sebastian.gc@globant.com>
2022-11-23 13:07:40 -0500

    ajustes home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div className='bg-slate-900 h-screen'>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </div>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,3 @@
-import Image from 'next/image';
-import logo from '../public/logo.png';
-
 export default function Home() {
-    return (
-        <div className="relative h-screen w-screen bg-[url('../public/wallpaper-christmas.jpg')] bg-cover bg-no-repeat">
-            <div className='absolute bottom-20 flex w-full flex-col content-end items-center justify-end gap-12 align-bottom'>
-                <Image src={logo} alt='SGCifuentes' />
-                <h1 className='text-center text-6xl text-white animate-pulse'>
-                    Merry Christmas!!!
-                </h1>
-            </div>
-        </div>
-    );
+    return <div className='bg-white'>Hola mundo</div>;
 }
